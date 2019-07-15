604819aae765053ee3953ec94e348488e5fa251d
clarabesset <clara.besset@gmail.com>
2019-07-15 12:27:56 +0200

    better form

--- blog-front/src/Pages/AddArticle.jsx
+++ blog-front/src/Pages/AddArticle.jsx
@@ -4,8 +4,10 @@ import FormAddArticle from "./../Components/FormAddArticle";
 export default class About extends Component {
   render() {
     return (
+      <div>
         <h2 className="articles-header-title">Add an article</h2>
         <FormAddArticle />
+      </div>
     );
   }
 }
