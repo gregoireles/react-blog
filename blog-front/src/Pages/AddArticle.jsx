a3429aab29fdfa1399e15ab700565c4a03d012df
clarabesset <clara.besset@gmail.com>
2019-07-15 12:27:10 +0200

    form article

---
+++ blog-front/src/Pages/AddArticle.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import FormAddArticle from "./../Components/FormAddArticle";
+
+export default class About extends Component {
+  render() {
+    return (
+        <h2 className="articles-header-title">Add an article</h2>
+        <FormAddArticle />
+    );
+  }
+}
