c19cd3f48a5feeffe21eeb2dfcff5aca616897ec
Camille Charpentier <cam.charpentier@live.fr>
2019-07-15 15:28:03 +0200

    updating form add article

--- blog-front/src/Components/FormAddArticle.jsx
+++ blog-front/src/Components/FormAddArticle.jsx
@@ -1,29 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 
-const FormAddArticle = ({ img }) => {
-  return (
+
+export default class FormAddArticle extends Component {
+  state = {
+    title: "",
+    highlight: "",
+    textbox: "",
+  };
+
+  handleChange = evt => {
+    console.log(evt.target.value)
+    const { title, highlight, textbox } = evt.target;
+    this.setState({ title: evt.target.value, highlight: evt.target.value, textbox: evt.target.value });
+  };
+
+  handleSubmit = evt => {
+    console.log(this.state)
+    evt.preventDefault();
+  };
+
+  render() {
+    return (
     <div>
-      <h2>Add an article</h2>
-      <label>
-        Title: <input type="text" name="title" />
-      </label>
-      <label>
-        Cover Images: <input type="file" name="imagecover" />
-      </label>
-      <label>
-        Content Images: <input type="file" name="imagecontent" />
-      </label>
-      <label>
-        Highlight: <input type="text" name="highlight" />
-      </label>
-      <label>
-        Text: <input type="text" name="textbox" />
-      </label>
+        <h2>Add an article</h2>
+        <form action="" onSubmit={this.handleSubmit} onChange={this.handleChange}>
+        <label>
+          Title: <input type="text" name="title"/>
+        </label>
+        <label>
+          Cover Images: <input type="file" name="imagecover" />
+        </label>
+        <label>
+          Content Images: <input type="file" name="imagecontent" />
+        </label>
+        <label>
+          Highlight: <input type="text" name="highlight" />
+        </label>
+        <label>
+          Text: <input type="text" name="textbox" />
+        </label>
       {/* <label>
         Type: <input type="text" name="type" />
       </label> */}
-    </div>
-  );
-};
-
-export default FormAddArticle;
+        <button>Create article</button>
+      </form>
+    </div>)
+  }
+}
