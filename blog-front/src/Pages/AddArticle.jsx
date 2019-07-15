91fde1f3246a5172d4c0dd3fd4c2b427a76fd976
clarabesset <clara.besset@gmail.com>
2019-07-15 14:03:51 +0200

    removed double title

--- blog-front/src/Pages/AddArticle.jsx
+++ blog-front/src/Pages/AddArticle.jsx
@@ -5,7 +5,6 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <h2 className="articles-header-title">Add an article</h2>
         <FormAddArticle />
       </div>
     );
