a82e67350666206ec7e33cf8e1727a59df70476a
clarabesset <clara.besset@gmail.com>
2019-07-15 12:48:11 +0200

    coucou

---
+++ blog-front/src/Pages/AddArticleCOUCOU.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import FormAddArticle from "./../Components/FormAddArticle";
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2 className="articles-header-title">Add an article</h2>
+        <FormAddArticle />
+      </div>
+    );
+  }
+}
